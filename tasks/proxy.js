27ad96ec9fe30d10746d69aa97137ac7b1c8ebf8
hushicai <bluthcy@gmail.com>
2013-04-08 15:55:32 +0800

    proxy logs

--- tasks/proxy.js
+++ tasks/proxy.js
@@ -12,12 +12,19 @@ module.exports = function(grunt) {
 	var httpProxy = require('http-proxy');
 	
 	grunt.registerMultiTask('proxy', 'Start proxy server', function() {
-		var options = this.options(); // TODO: defaults?
+		var options = this.options(); 
 		
-		if ( options.target )
+		if ( options.target ) {
 			httpProxy.createServer( options.target.port, options.target.host ).listen( options.port );
-		else if (options.router)
+            grunt.log.writeln('Starting proxy server on ' + options.target.host + ':' + options.target.port + '.');
+        } else if (options.router) {
 			httpProxy.createServer( { router:options.router} ).listen( options.port );
+            Object.keys(options.router).forEach(function(route) {
+                
+                grunt.log.writeln('Starting proxy router on "' + route + '" to "' + options.router[route] + '".');
+            
+            });
+        }
 	});
 	
 };
